1f358d90bd8907b6112cc2485f4845aa7f93735d
Ilya Kuznetsov <ilya@manticoresearch.com>
2021-05-02 13:48:50 +0300

    open files with FILE_SHARE_DELETE on windows

--- util/reader.cpp
+++ util/reader.cpp
@@ -81,7 +81,13 @@ FileReader_c::FileReader_c ( int iFD )
 
 bool FileReader_c::Open ( const std::string & sName, std::string & sError )
 {
+#ifdef _MSC_VER
+	HANDLE tHandle = CreateFile ( sName.c_str(), GENERIC_READ, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
+	m_iFD = _open_osfhandle ( (intptr_t)tHandle, 0 );
+#else
 	m_iFD = ::open ( sName.c_str(), O_RDONLY | O_BINARY, 0644 );
+#endif
+
 	if ( m_iFD<0 )
     {
         sError = FormatStr ( "error opening '%s': %s", sName.c_str(), strerror(errno) );
