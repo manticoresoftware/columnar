8e0b319f684e72065a0477500b1e04c242a9fd6a
Ilya Kuznetsov <ilya@manticoresearch.com>
2021-08-13 19:51:55 +0300

    fixed pread not being used when available; no FORCE_INLINE in debug

--- util/util.h
+++ util/util.h
@@ -37,19 +37,23 @@ namespace columnar
 #endif
 
 #ifndef FORCE_INLINE
-#  ifdef _MSC_VER
-#    define FORCE_INLINE __forceinline
-#  else
-#    if defined (__cplusplus) || defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L   /* C99 */
-#      ifdef __GNUC__
-#        define FORCE_INLINE inline __attribute__((always_inline))
-#      else
-#        define FORCE_INLINE inline
-#      endif
-#    else
-#      define FORCE_INLINE
-#    endif
-#  endif
+	#ifndef NDEBUG
+		#define FORCE_INLINE inline
+	#else
+		#ifdef _MSC_VER
+			#define FORCE_INLINE __forceinline
+		#else
+			#if defined (__cplusplus) || defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L   /* C99 */
+				#ifdef __GNUC__
+					#define FORCE_INLINE inline __attribute__((always_inline))
+				#else
+					#define FORCE_INLINE inline
+				#endif
+			#else
+				#define FORCE_INLINE
+			#endif
+		#endif
+	#endif
 #endif
 
 
