551006f2180d90859e96f92bcca613a78cfef189
Sergey Nikolaev <sergey@manticoresearch.com>
2021-12-01 07:29:16 +0100

    Debian 11 Bullseye support

---
+++ cmake/builds/build_bullseye.cmake
@@ -0,0 +1,6 @@
+# ---------- bullseye ----------
+# Above line is mandatory!
+# rules to build deb package for Debian Bullseye)
+
+message ( STATUS "Will create DEB for Debian Bullseye)" )
+include ( builds/CommonDeb )
--- cmake/builds/build_stretch.cmake
+++ cmake/builds/build_stretch.cmake
@@ -1,6 +1,6 @@
 # ---------- stretch ----------
 # Above line is mandatory!
-# rules to build deb package for Debian Buster)
+# rules to build deb package for Debian Stretch)
 
 message ( STATUS "Will create DEB for Debian Stretch)" )
 include ( builds/CommonDeb )
