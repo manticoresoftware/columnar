9ba093578672de9ed74c3a9e2b10eef8ce070bf0
Sergey Nikolaev <prostuda@academ.org>
2022-05-02 05:23:02 +0200

    fix to prev

---
+++ cmake/builds/build_jammy.cmake
@@ -0,0 +1,6 @@
+# ---------- jammy ----------
+# Above line is mandatory!
+# rules to build deb package for Ubuntu 22.04 (jammy)
+
+message ( STATUS "Will create DEB for Ubuntu 22.04 (jammy)" )
+include ( builds/CommonDeb )
