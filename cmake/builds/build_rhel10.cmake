4dbd7b53760d4c87384971e8a5b2467a1935bc25
djklim87 <klim@manticoresearch.com>
2025-06-30 14:41:59 +0200

    Feat: RHEL 10 support Ref: manticoresoftware/manticoresearch/issues/3426

---
+++ cmake/builds/build_rhel10.cmake
@@ -0,0 +1,7 @@
+# ---------- rhel10 ----------
+# Above line is mandatory!
+# rules to build rpm package for Red Hat linux 10 / Centos Stream 10
+
+message ( STATUS "Will create RPM for RedHat/Centos Stream 10" )
+set ( RELEASE_DIST ".el10" )
+include ( builds/CommonRpm )
